814673e4f0ebd09e70e1eb2d0e90ad5c30fcffcc
lorenzodinapoli <dinapoli.lorenzo99@gmail.com>
2022-06-23 12:17:16 -0700

    file manager automation project v1

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # python-scripting
 Quick python scripting projects to play around and deepen python knowledge.
 
-## 1. Image Processor
-Simple image processor that store potentially common changes to pictures to be run quickly when needed
+## File Management
+> Automation project to automatically move downloaded PDFs, images, and videos to specific folders
+
+
+Needs of the automation script:
+	1. File should be always running 
+	2. Script needs to be "listening" to changes in the downloads folder 
+	3. Send download to appropriate folder based on file type
+
+Requirements:
+	- Python needs to have access to file ["os" library]
+	- Do something when a new file appears ["Watchdog" library]
+
+Further improvements:
+	- Needs to be running all the time, right now the quick fix is a command line script that reduces the friction.
+	- Changing the name to a unique understandable one
---
+++ file_manager.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+
+
+# Setting directories we want to give access to
+source_dir = "/Users/lorenzodinapoli/Downloads"
+dest_dir_pdf = "/Users/lorenzodinapoli/Downloads/pdf"
+dest_dir_img = "/Users/lorenzodinapoli/Downloads/images&videos"
+
+
+def makeUnique(path):
+	"""
+	Creating a unique name for the moved file
+	"""
+	pass
+
+
+def move(dest, file, name):
+	"""
+	Checks that the file actually exists and moves it in the desired folder
+	"""
+	file_exists = os.path.exists(dest + "/" + name)
+	if file_exists:
+		#unique_name = makeUnique(name)
+		os.rename(file, name)
+	shutil.move(file, dest)
+
+
+class MoveHandler(FileSystemEventHandler):
+	"""
+	Class that manages the move of the file from Downloads to 
+	the desired folder.
+	"""
+	def on_modified(self, event):
+		# Giving access to the files in Downloads directory
+		with os.scandir(source_dir) as files:
+			for file in files:
+				name = file.name
+				dest = source_dir
+				# Directing files to destination
+				if name.endswith(".pdf"): # Moving PDFs
+					dest = dest_dir_pdf
+					move(dest, file, name)
+				elif name.endswith(".jpeg") or name.endswith(".png") 
+					or name.endswith(".jpg") or name.endswith(".mp4") or name.endswith(".mov"): # Moving images and videos
+					dest = dest_dir_img
+					move(dest, file, name)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoveHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+  	observer.join()
+
+
+
+
